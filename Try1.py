48fb3e7c45fa2f10ef97e2dbee6935fe52782a16
Jin Vega <jin-vega@yandex.ru>
2020-06-19 19:38:18 +0300

    test1

---
+++ Try1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+print('hello')
+
+table = pd.read_csv('StudentsPerformance.csv')
+#print(table)
+#table.head(10)
+print('hello git1??')
+print('hello git2??')
